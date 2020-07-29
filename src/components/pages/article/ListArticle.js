d7ce44bf1ef95baae02dadcdebee0216e9728d08
akbariyansyah <riyansean21366@gmail.com>
2020-07-29 21:43:36 +0700

    fixed pagination handler

--- src/components/pages/article/ListArticle.js
+++ src/components/pages/article/ListArticle.js
@@ -100,59 +100,69 @@ export default function ListArticle(props) {
                 {articles.allArticles.map(article =>
                     <div className="col-lg-12">
                         <h1 style={{ marginTop: 20 }}>{article.title_article}</h1>
-                        <p style={{ textAlign: "justify" }}>{article.content_article.substring(0,20)}...</p>
+                        <p style={{ textAlign: "justify" }}>{article.content_article.substring(0, 20)}...</p>
                         <p>posted : <span>{article.created_at}</span> </p>
-                            <Link to={{
-                                pathname: `/article/${article.user_id_article}/myarticles/${article.id_article}`
-                                ,state: {
-                                    title: article.title_article,
-                                    content: article.content_article,
-                                    id_article: article.id_article,
-                                    user_id_article: article.user_id_article
-                                }
-                            }}>
-                                <button type="submit" style={{ marginRight: 10 }} className="btn btn-outline-primary" onClick={() => edit({
-                                    title: article.title_article,
-                                    content: article.content_article,
-                                    id_article: article.id_article,
-                                    user_id_article: article.user_id_article
-                                })
-                                }>Read more</button>
-                            </Link>
-                            <button type="submit" style={{ marginRight: 10 }} className="btn btn-outline-info" data-toggle="modal" data-target="#exampleModal" onClick={() => edit({
+                        <Link to={{
+                            pathname: `/article/${article.user_id_article}/myarticles/${article.id_article}`
+                            , state: {
                                 title: article.title_article,
                                 content: article.content_article,
                                 id_article: article.id_article,
                                 user_id_article: article.user_id_article
-                            })}>edit</button>
-                            <button type="submit" className="btn btn-outline-danger"
-                                onClick={() => deletetion(article.id_article)}>
-                                delete
+                            }
+                        }}>
+                            <button type="submit" style={{ marginRight: 10 }} className="btn btn-outline-primary" onClick={() => edit({
+                                title: article.title_article,
+                                content: article.content_article,
+                                id_article: article.id_article,
+                                user_id_article: article.user_id_article
+                            })
+                            }>Read more</button>
+                        </Link>
+                        <button type="submit" style={{ marginRight: 10 }} className="btn btn-outline-info" data-toggle="modal" data-target="#exampleModal" onClick={() => edit({
+                            title: article.title_article,
+                            content: article.content_article,
+                            id_article: article.id_article,
+                            user_id_article: article.user_id_article
+                        })}>edit</button>
+                        <button type="submit" className="btn btn-outline-danger"
+                            onClick={() => deletetion(article.id_article)}>
+                            delete
                         </button>
                     </div>
                 )}
                 <EditArticleModal
-                            title={articles.title}
-                            content={articles.content}
-                            handleChange={handleChange}
-                            saveChange={save} />
-                    </div>
+                    title={articles.title}
+                    content={articles.content}
+                    handleChange={handleChange}
+                    saveChange={save} />
+            </div>
     }
-
+    let buttonPrev, buttonNext
+    if (page.offset < 1) {
+        buttonPrev = <button className="btn btn-outline-info" disabled>...</button>
+    } else {
+        buttonPrev = <button className="btn btn-outline-info" onClick={prev}>{page.offset}</button>
+    }
+    if (articles.allArticles.length < 3) {
+        buttonNext = <button className="btn btn-outline-info" disabled>...</button>
+    } else {
+        buttonNext = <button className="btn btn-outline-info" onClick={next}>{page.offset + 2}</button>
+    }
     return (
         <div className="container" id="list-article">
-                    <div className="row">
-                        <div className="col-lg-12 mb-4 border border-dark">
-                            <h1>My Articles</h1>
-                        </div>
-                    </div>
-                    {component}
-                    <button className="btn btn-success" onClick={prev}>{page.offset - 2}</button>
-                    <button className="btn btn-success" onClick={prev}>{page.offset - 1}</button>
-                    <button className="btn btn-primary">{page.offset}</button>
-                    <button className="btn btn-success" onClick={next}>{page.offset + 1}</button>
-                    <button className="btn btn-success" onClick={next}>{page.offset + 2}</button>
-
+            <div className="row">
+                <div className="col-lg-12 mb-4 border border-dark">
+                    <h1>My Articles</h1>
                 </div>
+            </div>
+            {component}
+            <div className="pagination">
+                {buttonPrev}
+                <button className="btn btn-primary">{page.offset + 1}</button>
+                {buttonNext}
+            </div>
+
+        </div>
     )
 }
